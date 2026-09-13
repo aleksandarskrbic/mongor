6061be8182e14afb34585c848ed89ee1cba8b8e3
aleksandarskrbic <aleksandarskrbic@users.noreply.github.com>
2026-09-13 10:49:23 +0200

    feat(cli): add clap subcommand skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "mongor", version, about)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Init,
+    New {
+        name: String,
+    },
+    Status,
+    Migrate
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Init => println!("init"),
+        Commands::New { name } => println!("new: {name}"),
+        Commands::Status => println!("status"),
+        Commands::Migrate => println!("migrate"),
+    }
 }
